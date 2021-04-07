36641b0057152a7c7f9c1def10a6bd5fa3221825
DESKTOP <nouha.jridiezzine@gmail.com>
2021-04-07 10:34:08 +0200

    initial commit

---
+++ BookApp/BookApp/Controllers/BillboardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Controllers
+{
+    public class BillboardController : Controller
+    {
+        public string GetBillboards()
+        {
+            return "All Ad Billboard";
+        }
+        public string GetBillboard(int id)
+        {
+            return $"Add One Billboard with id ={id}";
+        }
+        public string SearchBillboard(string billboardName, string billboardOwner)
+        {
+            return $"Add Billboard titled: {billboardName} setted by {billboardOwner}";
+        }
+    }
+}
---
+++ BookApp/BookApp/Models/BillboardModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Models
+{
+    public class BillboardModel
+    {
+        public int Id { get; set; }
+
+        public string BillboardName { get; set; }
+        public string BillboardOwner { get; set; }
+    }
+}
